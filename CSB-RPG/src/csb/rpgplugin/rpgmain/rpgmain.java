eb2e5334d6dab7cadf40894f240d42da3c79c5e9
Yordie <y.santana.sanz@gmail.com>
2013-01-07 22:26:10 +0100

    CommitTest

--- CSB-RPG/src/csb/rpgplugin/rpgmain/rpgmain.java
+++ CSB-RPG/src/csb/rpgplugin/rpgmain/rpgmain.java
@@ -5,8 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class rpgmain extends JavaPlugin{
 	@Override
     public void onEnable(){
-        // TODO Insert logic to be performed when the plugin is enabled
-    }
+		getLogger().info("onEnable has been invoked!");
+		}
  
     @Override
     public void onDisable() {
